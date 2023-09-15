f8c6a9f9651b7c69e6b32357074ad2d63e96b047
sukhvir-singh3 <sukhvir-singh3@gmail.com>
2023-09-15 16:31:35 -0400

    Test pushing to Mobile

--- dynamicapp/components/confetti.js
+++ dynamicapp/components/confetti.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import useWindowSize from 'react-use/lib/useWindowSize'
-import Confetti from 'react-confetti'
+import React from 'react';
+import useWindowSize from 'react-use/lib/useWindowSize';
+import Confetti from 'react-confetti';
 
-export default () => {
-  const { width, height } = useWindowSize()
-  return (
-    <Confetti
-      width={width}
-      height={height}
-    />
-  )
-}
+const ConfettiComponent = () => {
+  const { width, height } = useWindowSize();
+  return <Confetti width={width} height={height} />;
+};
+
+ConfettiComponent.displayName = 'ConfettiComponent'; // Add a display name
+
+export default ConfettiComponent;
