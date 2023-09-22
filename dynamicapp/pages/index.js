349e7111143c64db3663b069a61c7463e73a43a4
sukhvir-singh3 <sukhvir-singh3@gmail.com>
2023-09-22 19:09:35 -0400

    correvtng the turn logic after winning

--- dynamicapp/pages/index.js
+++ dynamicapp/pages/index.js
@@ -3,15 +3,27 @@ import Router from "next/router";
 import Link from "next/link";
 
 export default function Home() {
-    const handleButtonClick = () => {
-        // Use Router.push to navigate to the "Form" component
-        Router.push("/form");
-    };
-
     return (
         <>
             <div>
-                <CardGroup style={{ alignItems: "center", justifyContent:"center", position:"relative", top:"10vh" }}>
+                <div
+                    style={{
+                        color: "#0000EE",
+                        position: "relative",
+                        top: "4vh",
+                        fontSize:"20px"
+                    }}
+                >
+                    Select A Game Mode
+                </div>
+                <CardGroup
+                    style={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                        position: "relative",
+                        top: "10vh",
+                    }}
+                >
                     <Link href="/form" gameMode="BOT">
                         <Card className="SelectGameModeCard">
                             <Card.Body className="d-flex flex-column align-items-center justify-content-center">
